d66d6e3c8b188da1d7accd0abe557e8da4304186
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-06 14:08:32 +0300

    results ok

--- main.py
+++ main.py
@@ -31,6 +31,25 @@ calculate_button.pack()
 result_label = tkinter.Label()
 result_label.pack()
 
+def write_result(bmi):
+    result_string = f"Your BMI is {round(bmi, 2)}. You are "
+    if bmi <= 16:
+        result_string += "severely thin!"
+    elif 16 < bmi <= 17:
+        result_string += "moderately thin!"
+    elif 17 < bmi <= 18.5:
+        result_string += "mild thin!"
+    elif 18.5 < bmi <= 25:
+        result_string += "normal weight"
+    elif 25 < bmi <= 30:
+        result_string += "overweight"
+    elif 30 < bmi <= 35:
+        result_string += "obese class 1"
+    elif 35 < bmi <= 40:
+        result_string += "obese class 2"
+    else:
+        result_string += "obese class 3"
+    return result_string
 
 
 window.mainloop()
